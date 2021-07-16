1227e78df1a8439537fc8c1e41a4521e70ec2926
user <user@DESKTOP-7B02D51>
2021-07-16 18:06:12 +0900

    올라가요~

--- Text1/src/Text1/Text1.java
+++ Text1/src/Text1/Text1.java
@@ -3,6 +3,7 @@ package Text1;
 public class Text1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("하하");
+		System.out.println("�븯�븯");
+		System.out.println("안녕 새로만들엇당");
 	}
 }
